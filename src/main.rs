d49fbdf40a91f17447a354442af2692b3eb84fec
kmatasfp <33095685+kmatasfp@users.noreply.github.com>
2024-12-15 03:14:16 -0800

    The type builtin: executable files

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::{
+    env::{self, VarError},
+    fs::ReadDir,
+    path::Path,
+};
+use std::{fs, path::PathBuf};
 
-fn main() {
+fn main() -> Result<()> {
     loop {
         print!("$ ");
-        io::stdout().flush().unwrap();
+        io::stdout().flush()?;
 
         // Wait for user input
         let stdin = io::stdin();
         let mut input = String::new();
-        stdin.read_line(&mut input).unwrap();
+        stdin.read_line(&mut input)?;
 
         let trimmed_input = input.trim();
 
@@ -25,7 +31,17 @@ fn main() {
                     if builtin_commands.contains(&command) {
                         println!("{} is a shell builtin", command)
                     } else {
-                        println!("{}: not found", command)
+                        let executables = executables_on_path()?;
+
+                        let maybe_executable = executables
+                            .into_iter()
+                            .find(|executable| executable.starts_with(command));
+
+                        if let Some(executable) = maybe_executable {
+                            println!("{} is {}", command, executable.display())
+                        } else {
+                            println!("{}: not found", command)
+                        }
                     }
                 } else {
                     println!("{}: not found", i)
@@ -33,4 +49,58 @@ fn main() {
             }
         }
     }
+    Ok(())
 }
+
+fn executables_on_path() -> Result<Vec<PathBuf>> {
+    let path_var = "PATH";
+
+    let value = env::var(path_var)?;
+
+    println!("PATH is {}", value);
+
+    let dirs: Vec<&Path> = value.split(":").map(Path::new).collect();
+
+    files_in_dirs(dirs).map_err(Error::Io)
+}
+
+fn files_in_dirs(dirs: Vec<&Path>) -> io::Result<Vec<PathBuf>> {
+    dirs.iter()
+        .map(|dir| fs::read_dir(dir).and_then(files_in_dir))
+        .collect::<io::Result<Vec<_>>>()
+        .map(|f| f.into_iter().flatten().collect())
+}
+
+fn files_in_dir(dir: ReadDir) -> io::Result<Vec<PathBuf>> {
+    dir.map(|res| res.map(|e| e.path()))
+        .collect::<io::Result<Vec<_>>>()
+}
+
+pub type Result<T> = core::result::Result<T, Error>;
+
+#[derive(Debug)]
+#[allow(dead_code)]
+pub enum Error {
+    EnvVarError(VarError),
+    Io(std::io::Error),
+}
+
+impl From<VarError> for Error {
+    fn from(value: VarError) -> Self {
+        Self::EnvVarError(value)
+    }
+}
+
+impl From<std::io::Error> for Error {
+    fn from(value: std::io::Error) -> Self {
+        Self::Io(value)
+    }
+}
+
+impl core::fmt::Display for Error {
+    fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(fmt, "{self:?}")
+    }
+}
+
+impl std::error::Error for Error {}
